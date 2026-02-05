3fd4d9e19ff669cbf553f2e19b6919997307e9d6
Abhik Sarkar <abhiksark@gmail.com>
2026-02-05 17:46:33 +0530

    fix: synchronize version across all files

--- libs/__init__.py
+++ libs/__init__.py
@@ -1,8 +1,8 @@
 # libs/__init__.py
 """LabelImg++ library package."""
 
-__version_info__ = ('2', '1', '0b')
-__version__ = '2.1.0b'
+__version_info__ = ('2', '1', '1')
+__version__ = '2.1.1'
 
 # Re-export subpackages for convenient access
 from libs import core, formats, widgets, utils
